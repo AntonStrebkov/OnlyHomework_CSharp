2aef415a78e4fe15947772500972ece652057581
AntonStrebkov <wancler987@gmail.com>
2022-09-02 12:32:36 +0700

    Add homework 9

---
+++ Homework_CSharp/Lecture_9/Task_066/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumElements(int start, int end)
+{
+    if (start > end) return 0;
+
+    return start + SumElements(start + 1, end);
+}
+
+System.Console.WriteLine(SumElements(4, 8));
---
+++ Homework_CSharp/Lecture_9/Task_068/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 29
+
+int FuctionAkkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return FuctionAkkerman(m - 1, 1);
+    else
+        return FuctionAkkerman(m - 1, FuctionAkkerman(m, n - 1));
+}
+
+Console.WriteLine(FuctionAkkerman(2, 3));
